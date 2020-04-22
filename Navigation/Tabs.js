60c146e9ecc7d92eeae3d5afe842322df744f1e0
taehunlim <dkslsdkdl@naver.com>
2020-04-22 21:03:54 +0900

    tabBar Styling

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useLayoutEffect} from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
+import { Platform } from 'react-native';
 import Movies from "../Screens/Movies";
 import TV from "../Screens/TV";
 import Search from "../Screens/Search";
 import Favs from "../Screens/Favs";
-import {get} from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 
 const Tabs = createBottomTabNavigator();
@@ -21,11 +22,41 @@ export default ({navigation, route}) => {
     }, [route]);
 
     return (
-        <Tabs.Navigator>
+        <Tabs.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon : ({focused}) => {
+                    let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+                    if (route.name === "Movies") {
+                        iconName += "film";
+                    } else if (route.name === "TV") {
+                        iconName += 'tv';
+                    } else if (route.name === "Search") {
+                        iconName += "search";
+                    } else if (route.name === "Favs") {
+                        iconName += "heart";
+                    }
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            color={focused ? "white" : "grey"}
+                            size={26}
+                        />
+                    )
+                }
+            })}
+            tabBarOptions={{
+                showLabel : false,
+                style: {
+                    backgroundColor : "black",
+                    borderTopColor : "black"
+                }
+            }}
+        >
             <Tabs.Screen name="Movies" component={Movies}/>
             <Tabs.Screen name="TV" component={TV}/>
-            <Tabs.Screen name="Search" component={Search}/>
             <Tabs.Screen name="Favs" component={Favs}/>
+            <Tabs.Screen name="Search" component={Search}/>
+
         </Tabs.Navigator>
     )
 };
