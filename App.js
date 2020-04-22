c031b0a4375ea7820893e537c9e849c88dcf7648
taehunlim <dkslsdkdl@naver.com>
2020-04-22 20:01:35 +0900

    cacheFonts

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
 import {AppLoading} from 'expo';
+import * as Font from 'expo-font';
 import {View, Text, Image} from "react-native";
 import {Asset} from 'expo-asset';
+import {Ionicons} from '@expo/vector-icons';
 
 
 const cacheImage = images =>
@@ -14,21 +16,29 @@ const cacheImage = images =>
         }
     })
 
+const cacheFonts = fonts => (
+    fonts.map(font => [Font.loadAsync(font), Font.loadAsync(font)])
+)
+
 export default function App() {
 
     const [isReady, setIsReady] = useState(false);
-    const loadAssets = async () => {
+    const loadAssets = () => {
         const image = cacheImage([
-            "https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            "https://images.unsplash.com/photo-1562887189-e5d078343de4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             require("./assets/splash.png")
         ]);
         console.log(images)
+        const fonts = cacheFonts([Ionicons.font])
+        return Promise.all([...images, ...fonts])
     };
 
     const onFinish = () => setIsReady(true);
 
     return isReady ? (
-        <Text>Ready!</Text>
+        <View>
+            <Text>Ready!</Text>
+        </View>
     ) : (
         <AppLoading
             startSync={loadAssets}
