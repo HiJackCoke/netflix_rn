6298f41650389fc7e5e649a57cb4b0391fb3f26a
taehunlim <dkslsdkdl@naver.com>
2020-04-22 21:21:03 +0900

    Api(TMDB) #1

---
+++ api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+
+const TMDB_KEY = "da8324cd688ff9706ecae7f17f146e85";
+
+const makeRequest = (path, params) =>
+    axios.get(`https://api.themoviedb.org/3${path}`, {
+        params : {
+            ...params,
+            api_key : TMDB_KEY
+        }
+    });
+
+export const movieAPI = {
+    nowPlaying : () => makeRequest("/movie/now_playing"),
+    popular : () => makeRequest("/movie/popular"),
+    upcoming : () => makeRequest("/movie/upcoming", {region: "kr"})
+};
+
+export const tvAPI = {
+    today : () => makeRequest('/tv/airing_today'),
+    thisWeek : () => makeRequest('/tv/on_the_air'),
+    topRated : () => makeRequest('/tv/top_rated'),
+    popular : () => makeRequest('/tv/popular')
+};
