84227e96f5b385937ba7f35e00403e8e377499a8
taehunlim <dkslsdkdl@naver.com>
2020-04-22 19:37:30 +0900

    change class component

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends Component {
+    render() {
+        return (
+            <View>
+              <Text>wouyuygooihoioiurk</Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
